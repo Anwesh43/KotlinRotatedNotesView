79328383dc1d7efc4cd2e7096bd97166d9c5c252
Anwesh43 <anweshthecool0@gmail.com>
2018-05-06 11:36:35 +0530

    adding fullScreen support

--- app/src/main/java/com/example/anweshmishra/kotlinrotatednotesview/MainActivity.kt
+++ app/src/main/java/com/example/anweshmishra/kotlinrotatednotesview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.anweshmishra.kotlinrotatednotesview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.example.rotatednotesview.RotatedNotesView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         RotatedNotesView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+    window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 }
