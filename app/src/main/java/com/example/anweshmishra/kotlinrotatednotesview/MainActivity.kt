d29fef9598c6407fd7ba51eb9b961521b5089a66
Anwesh43 <anweshthecool0@gmail.com>
2018-05-06 11:15:56 +0530

    adding View in Activity

--- app/src/main/java/com/example/anweshmishra/kotlinrotatednotesview/MainActivity.kt
+++ app/src/main/java/com/example/anweshmishra/kotlinrotatednotesview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.anweshmishra.kotlinrotatednotesview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.rotatednotesview.RotatedNotesView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RotatedNotesView.create(this)
     }
 }
