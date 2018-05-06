0ba18616ddf8fef0b67f6d1fc3bc28186b7491e6
Anwesh43 <anweshthecool0@gmail.com>
2018-05-06 10:14:26 +0530

    adding rotatednotesview module

--- rotatednotesview/src/main/java/com/example/rotatednotesview/RotatedNotesView.kt
+++ rotatednotesview/src/main/java/com/example/rotatednotesview/RotatedNotesView.kt
@@ -3,3 +3,27 @@ package com.example.rotatednotesview
 /**
  * Created by anweshmishra on 06/05/18.
  */
+
+import android.graphics.*
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+
+class RotatedNotesView (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+
+}
